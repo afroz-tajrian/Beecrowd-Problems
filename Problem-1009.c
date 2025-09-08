f5d7cf94fdef21bee9db0ed22ad40005c47ed794
afroz-tajrian <tajrianafroz60@gmail.com>
2025-09-09 00:01:53 +0600

    Add files via upload

---
+++ Problem-1009.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main() {
+    char name[100];
+    double salary, sales, total;
+    scanf("%s %lf %lf", name, &salary, &sales);
+    total = salary + 0.15 * sales;
+    printf("TOTAL = R$ %.2lf\n", total);
+
+    return 0;
+}
